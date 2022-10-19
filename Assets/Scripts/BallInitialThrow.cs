e840cb3197afa96d393206df402a5ae49e484cc4
Tom Jackson <tjackson5@ltu.edu>
2022-10-19 17:17:56 -0400

    Created Ball and Planet Scripts

---
+++ Assets/Scripts/BallInitialThrow.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BallInitialThrow : MonoBehaviour
+{
+    public Collider2D ball;
+    public Vector2 motion = Vector2.left;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
