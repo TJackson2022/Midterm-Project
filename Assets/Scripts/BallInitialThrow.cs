c99cdb5203a3fd733db722aad7fcddfaffad08da
Tom Jackson <tjackson5@ltu.edu>
2022-10-26 16:33:10 -0400

    Moved Assets and Added Space Assets
    Some small work on scripts too

--- Assets/Scripts/BallInitialThrow.cs
+++ Assets/Scripts/BallInitialThrow.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.AnimatedValues;
 using UnityEngine;
+using UnityEngine.Windows;
 
 public class BallInitialThrow : MonoBehaviour
 {
-    public Collider2D ball;
+    public Rigidbody2D ball;
     public Vector2 motion = Vector2.left;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ball.AddForce(motion);
     }
 
     // Update is called once per frame
